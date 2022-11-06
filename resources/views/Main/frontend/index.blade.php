f27fe8b2aa2ac326b1f81b7c456bd3921b75ed9c
HaiderZia96 <develophaider11@gmail.com>
2022-11-07 01:46:43 +0500

    1.46 am update 11/7/2022

--- resources/views/Main/frontend/index.blade.php
+++ resources/views/Main/frontend/index.blade.php
@@ -5,4 +5,196 @@
             <img src="{{asset('Main/frontend/images/cambridge.png')}}">
         </div>
 </div>
+<div class="home-wrapper">
+<div class="container">
+    <div class="row mt-3 g-0">
+        <div class="col-lg-5">
+            <div class="text-cambridge">
+                <h1>Welcome to International School</h1>
+                <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+            <div class="d-sm-flex two-logo">
+            <img src="{{asset('Main/frontend/images/kembridz.png')}}" class="me-lg-3">
+            <img src="{{asset('Main/frontend/images/ib.png')}}" class="me-lg-3 ">
+            </div>
+            </div>
+<div class="col-lg-7">
+            <div class="building-flex text-center mb-5">
+            <img src="{{asset('Main/frontend/images/blocks.png')}}" class="img-fluid">
+            <img src="{{asset('Main/frontend/images/group.png')}}" class="img-fluid group-image">
+            </div>
+            </div>
+
+    </div></div>
+    <div class="container">
+        <div class="row g-0 my-3">
+            <div class="col-md-6">
+        <div class="video-section mx-sm-2">
+        <iframe
+        src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+        </iframe>
+</div></div>
+<div class="col-md-6">
+        <div class="video-section mx-sm-2">
+        <iframe
+        src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+        </iframe>
+</div></div>
+</div>
+</div>
+<div class="container">
+<div class="text-center endorse">
+        <h1>our endoresments</h1>
+</div>
+<div class="row g-0 justify-content-evenly text-center">
+
+<div class="col-lg-1 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/ucas.jpg')}}" class="img-fluid">
+</div>
+<div class="col-lg-1 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/sat.jpg')}}" class="img-fluid">
+</div>
+<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/ib.png')}}" class="img-fluid">
+</div>
+<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/ministar.jpg')}}" class="img-fluid">
+</div>
+<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/cam.jpg')}}" class="img-fluid">
+</div>
+<div class="col-lg-1 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/act.jpg')}}" class="img-fluid">
+</div>
+<div class="col-lg-1 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/vaslika.png')}}" class="img-fluid">
+</div>
+<div class="col-lg-2 col-md-3 col-sm-4 col-6">
+    <img src="{{asset('Main/frontend/images/erasmus.png')}}" class="img-fluid">
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row g-0">
+        <div class="truely-text text-center">
+            <h5><span>Truely different</span></h5>
+</div>
+<div class="modern-text text-center">
+    <h5 class="my-4">Modern education in every sense of the world</h5>
+    <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</div>
+</div>
+</div>
+
+
+<div class="abc-work ">
+<div class="container-fluid g-0">
+<div class="row g-0 justify-content-center mx-5">
+<div class="owl-carousel owl-theme">
+<div class="item">
+<div class="col-lg-12">
+<div class="card me-lg-3 mt-2">
+<div class="image-one">
+<img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid" >
+</div>
+<div class="d-inline mt-2 mx-2">
+ <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+</div>
+</div>
+</div>
+</div>
+
+<div class="item">
+<div class="col-lg-12">
+    <div class="card me-lg-3 mt-2">
+    <div class="image ">
+    <img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+    </div>
+    <div class="d-inline mt-2 mx-2">
+     <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+    </div>
+    </div>
+</div></div>
+
+<div class="item">
+<div class="col-lg-12">
+    <div class="card  me-lg-3 mt-2">
+    <div class="image ">
+    <img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+    </div>
+    <div class="d-inline mt-2 mx-2">
+     <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+    </div>
+    </div>
+</div></div>
+
+<div class="item">
+<div class="col-lg-12">
+    <div class="card  me-lg-3 mt-2">
+    <div class="image ">
+    <img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+    </div>
+    <div class="d-inline mt-2 mx-2">
+     <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+    </div>
+    </div>
+</div></div>
+</div>
+</div>
+</div>
+</div>
+<div class="team-image my-3 g-0">
+<img src="{{asset('Main/frontend/images/team_work.png')}}" class="img-fluid">
+</div>
+<div class="latest-news my-3 g-0">
+<h5 class="text-center">Latest news from internaional school</h5>
+<div class="latest-news-cards ">
+<div class="container-fluid g-0">
+<div class="row g-0 justify-content-center mx-5">
+
+<div class="col-lg-4">
+<div class="card one me-lg-3 mt-2">
+<div class="image-one">
+<img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+</div>
+<div class="d-inline mt-2 mx-2">
+ <h4>Data One</h4>
+ <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+</div>
+</div>
+</div>
+
+
+
+<div class="col-lg-4">
+    <div class="card two me-lg-3 mt-2">
+    <div class="image-two ">
+    <img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+    </div>
+    <div class="d-inline mt-2 mx-2">
+     <h4>Data Two</h4>
+     <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+    </div>
+    </div>
+</div>
+
+
+<div class="col-lg-4">
+    <div class="card three me-lg-3 mt-2">
+    <div class="image-three ">
+    <img src="{{asset('Main/frontend/images/chairs.png')}}" class="img-fluid">
+    </div>
+    <div class="d-inline mt-2 mx-2">
+     <h4>Data Three</h4>
+     <h6>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</h6>   
+    </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 @endsection
--- resources/views/Main/frontend/scripts.blade.php
+++ resources/views/Main/frontend/scripts.blade.php
@@ -26,13 +26,13 @@
 
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-{{-- <script>
+<script>
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: false,
         dots: false,
-        autoplay:true,
+        autoplay:false,
         responsive: {
             0: {
                 items: 1
@@ -41,17 +41,17 @@
                 items: 1
             },
             600: {
-                items: 1
+                items: 2
             },
             1000: {
-                items: 1
+                items: 4
             },
             1300: {
-                items: 1
+                items: 4
             },
         }
     })
-</script> --}}
+</script> 
 @stack('js')
 </body>
 </html>
